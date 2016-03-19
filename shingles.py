0a60abc834c09e4d8010743e1170ce8be6c17ffb
pavel <itasarom@gmail.com>
2016-03-19 10:31:03 +0300

    added correct reading

--- shingles.py
+++ shingles.py
@@ -11,20 +11,9 @@ import mmh3
 import google.protobuf as protobuf
 import subprocess
 
-k = 10
-
-word_re = re.compile(r'\w+', flags = re.UNICODE)
+import time
 
 
-htmls = []
-
-shls = []
-files = []
-
-	# prefix = "./dataset"
-	# for filename in os.listdir(prefix):
-
-# from io import BufferedReader as Reader
 
 from src import document_pb2
 import struct
@@ -49,10 +38,10 @@ class DocumentStreamReader:
 
 
 
-def read_documents(path):
+def read_documents(file_list):
 
-	files = [os.path.join(path, f) for f in os.listdir(path)]
-	zcat = subprocess.Popen(["zcat"] + files, stdout=subprocess.PIPE)
+	# files = [os.path.join(path, f) for f in file_list]
+	zcat = subprocess.Popen(["zcat"] + file_list, stdout=subprocess.PIPE)
 	reader = DocumentStreamReader(zcat.stdout)
 	for document in reader:
 		yield document.url, document.text
@@ -66,53 +55,84 @@ def get_draft(doc, k, w):
 
 	draft = set()
 	for i in range(w):
-		draft |= {min(baskets[i])}
+		if baskets[i]:
+			draft |= {min(baskets[i])}
 
 	
 	return draft
 
 
-def get_sets(k = 5, w = 20):
-	path = "./dataset"
+def get_sets(file_list, k = 5, w = 20):
 
 	ids = dict()
 	sets = []
 	i = 0
-	for url, doc in read_documents(path):
+	for url, doc in read_documents(file_list):
 		sets.append(get_draft(doc, k, w))
 		ids[i] = url
+		# if i % 100 == 0:
+		# 	print "Got sets {0} urls".format(i)
 		i += 1
+		# if i % 100 == 0:
+		# 	print i
+	# print "Got sets!"
+	return sets, ids
 
-	return sets
-
-def get_shingles(doc, k = 5):
+word_re = re.compile(r'\w+', flags = re.UNICODE)
+def get_shingles(doc, k):
 	words = re.findall(word_re, doc)
 	for i in xrange(len(words) - k):
 		yield " ".join(words[i:i + k])
 
 
 
-print get_sets()
+
+def get_similarities(file_list):
+	timestamp = time.time()
+	sets, ids = get_sets(file_list)
+
+	result = []	
+	# print "Going to find similarities"
+	total = 0
+	for i in xrange(len(sets)):
+		for j in xrange(i + 1, len(sets)):
+			if len(sets[i]) == 0 and len(sets[j]) == 0:
+				continue
+			# if total % 10000 == 0:
+				# print "Processed {0} of {1}".format(total,  len(sets)**2)
+
+			jacc = len(sets[i] & sets[j]) * 1.0 / len(sets[i] | sets[j])
+			if jacc > 0.6:
+				result.append((i, j, jacc))
+
+			total += 1
+
+
+	# print "Found {0} similarities in {1} seconds".format(len(result), time.time() - timestamp)
+	result = [(ids[it[0]], ids[it[1]], it[2]) for it in result]
+	return result
+
+
+def process(file_list):
+	result = get_similarities(file_list)
+
+	for it in result:
+		# print "'{0}' ~ '{1}' == {2}".format(it[0], it[1], it[2])
+		print "{0} {1} {2}".format(it[0], it[1], it[2])
 
 
 
-# for i in xrange(len(words) - k):
-# 	cur_shingle = words[i:i + k]
-# 	shingles |= {" ".join(cur_shingle)}
+def main():
+	path = "./dataset"
+	if len(sys.argv) == 1:
+		file_list = [os.path.join(path, f) for f in os.listdir(path)]
 
-# shls.append(shingles)
+	else:
+		file_list = [sys.argv[1]]
+	process(file_list)
 
-
-# for i in xrange(len(shls)):
-# 	for j in xrange(i + 1, len(shls)):
-
-# 		jacc = len(shls[i] & shls[j]) * 1.0 / len(shls[i] | shls[j])
-
-# 		if jacc > 0.5:
-# 			print files[i], files[j]
-
-
-
+if __name__ == "__main__":
+	main()
 
 
 
